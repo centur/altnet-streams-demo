1df9fd5b888048e10b26c5b1420206f0f3a7462e
Alexey Shcherbak <alexey@drawboard.com>
2017-09-24 17:08:14 +1000

    add utils project with helpers

--- OrleansClient/Program.cs
+++ OrleansClient/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Orleans;
+using Orleans.Runtime.Configuration;
+using Utils;
 
 namespace OrleansClient
 {
@@ -10,6 +11,47 @@ namespace OrleansClient
 	{
 		static void Main(string[] args)
 		{
+			var clientInstance = InitializeClient(args).GetAwaiter().GetResult();
+
+			PrettyConsole.Line("==== CLIENT: Initialized ====");
+			PrettyConsole.Line("CLIENT: Write commands:", ConsoleColor.Green);
+
+			Console.ReadLine();
+		}
+
+		static async Task<IClusterClient> InitializeClient(string[] args)
+		{
+			int initializeCounter = 0;
+
+			var config = ClientConfiguration.LocalhostSilo();
+			var builder = new ClientBuilder().UseConfiguration(config);
+
+			var initSucceed = true;
+			while (initSucceed)
+			{
+
+				try
+				{
+					var client = builder.Build();
+					await client.Connect();
+					return client;
+				}
+				catch (Exception exc)
+				{
+					PrettyConsole.Line(exc.Message, ConsoleColor.Cyan);
+					initSucceed = false;
+				}
+
+				if (initializeCounter++ > 10)
+				{
+					return null;
+				}
+
+				PrettyConsole.Line("Client Init Failed. Sleeping 5s...", ConsoleColor.Red);
+				Thread.Sleep(TimeSpan.FromSeconds(5));
+			}
+
+			return null;
 		}
 	}
 }
--- OrleansServer/Program.cs
+++ OrleansServer/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Orleans.Runtime.Configuration;
+using Orleans.Runtime.Host;
 
 namespace OrleansServer
 {
@@ -10,6 +8,19 @@ namespace OrleansServer
 	{
 		static void Main(string[] args)
 		{
+
+			var siloConfig = ClusterConfiguration.LocalhostPrimarySilo();
+
+
+			var silo = new SiloHost("Alt.NET Demo Silo", siloConfig);
+			silo.InitializeOrleansSilo();
+			silo.StartOrleansSilo();
+
+			Console.WriteLine("Press Enter to close.");
+			Console.ReadLine();
+
+			// shut the silo down after we are done.
+			silo.ShutdownOrleansSilo();
 		}
 	}
 }

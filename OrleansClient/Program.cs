912e84e729a14c8ae9bfac044103291ee33dac01
Alexey Shcherbak <alexey@drawboard.com>
2017-09-24 18:03:06 +1000

    primitive menu and handlers

--- OrleansClient/Program.cs
+++ OrleansClient/Program.cs
@@ -9,14 +9,18 @@ namespace OrleansClient
 {
 	class Program
 	{
+		private static string _joinedChannel = "#general";
+
 		static void Main(string[] args)
 		{
 			var clientInstance = InitializeClient(args).GetAwaiter().GetResult();
 
-			PrettyConsole.Line("==== CLIENT: Initialized ====");
-			PrettyConsole.Line("CLIENT: Write commands:", ConsoleColor.Green);
+			PrettyConsole.Line("==== CLIENT: Initialized ====", ConsoleColor.Cyan);
+			PrettyConsole.Line("CLIENT: Write commands:", ConsoleColor.Cyan);
 
-			Console.ReadLine();
+			Menu(clientInstance);
+
+			PrettyConsole.Line("==== CLIENT: Shutting down ====", ConsoleColor.DarkRed);
 		}
 
 		static async Task<IClusterClient> InitializeClient(string[] args)
@@ -29,7 +33,6 @@ namespace OrleansClient
 			var initSucceed = true;
 			while (initSucceed)
 			{
-
 				try
 				{
 					var client = builder.Build();
@@ -52,6 +55,47 @@ namespace OrleansClient
 			}
 
 			return null;
+		}
+
+		private static void PrintHints()
+		{
+			var menuColor = ConsoleColor.Magenta;
+			PrettyConsole.Line("Type '/join <channel>' to join specific channel", menuColor);
+			PrettyConsole.Line("Type '<message>' to send a message", menuColor);
+			PrettyConsole.Line("Type '/exit' to exit client.", menuColor);
+		}
+
+		private static void Menu(IClusterClient client)
+		{
+			string input;
+			do
+			{
+				PrintHints();
+
+				input = Console.ReadLine();
+
+				if (string.IsNullOrWhiteSpace(input)) continue;
+
+				if (input.StartsWith("/join"))
+				{
+					var channelName = input.Replace("/join ", "");
+					JoinChannel(client, channelName);
+				}
+				else
+				{
+					SendMessage(client, input, _joinedChannel);
+				}
+			} while (input != "/exit");
+		}
+
+		private static void SendMessage(IClusterClient client, string input, string joinedChannel)
+		{
+			PrettyConsole.Line($"Sending '{input}' to channel {joinedChannel}");
+		}
+
+		private static void JoinChannel(IClusterClient client, string channelName)
+		{
+			PrettyConsole.Line($"Joining to channel {channelName}");
 		}
 	}
 }
